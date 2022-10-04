0fb607944b0a11d995290bd19f3a9987f3133cd7
MuriungiMartin <MuriungiMmwiti@gmail.com>
2022-10-04 12:18:17 +0300

    models and mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require('express');
+const authRouter=require("./routes/auth");
+const mongoose = require("mongoose");
+const e = require('express');
+
+const port = 3000;
+const app = express();
+const DB = "mongodb+srv://Martin:Cephajosh3@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
+app.use(authRouter);
+mongoose.connect(DB).then(()=>{
+    console.log("Connection successful");
+}).catch(e =>{
+    console.log(e);
+});
+
+app.listen(port,() => {
+    console.log(`connected at port ${port}`)
+});
