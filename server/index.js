f7ffec078c6087b68379acc74dddf7f516488a31
MuriungiMartin <39697319+MuriungiMartin@users.noreply.github.com>
2022-10-05 22:39:26 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const e = require('express');
 
 const port = 3000;
 const app = express();
-const DB = "mongodb+srv://Martin:Cephajosh3@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://name:password@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
 app.use(authRouter);
 mongoose.connect(DB).then(()=>{
     console.log("Connection successful");
