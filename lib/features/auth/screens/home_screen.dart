75b1ea0fabbe4fa6032ccc93be542d45694fc1be
MuriungiMartin <MuriungiMmwiti@gmail.com>
2022-10-12 20:39:43 +0300

    UI skeleton to build on

---
+++ lib/features/auth/screens/home_screen.dart
@@ -0,0 +1,96 @@
+// ignore_for_file: must_call_super
+
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smartfarmke/common/widgets/custom_app_bar.dart';
+import 'package:smartfarmke/cubits/cubits.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late ScrollController _scrollController;
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        //   context.read<AppBarCubit>(_scrollController.offset);
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightGreenAccent,
+        onPressed: (() => print('object')),
+      ),
+      appBar: PreferredSize(
+        preferredSize: Size(screenSize.width, 50.0),
+        child:
+            BlocBuilder<AppBarCubit, double>(builder: ((context, scrollOffset) {
+          return CustomAppBar(scrollOffset: scrollOffset);
+        })),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                key: PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('myList'),
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              key: PageStorageKey('originals'),
+              title: 'Netflix Originals',
+              contentList: originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                key: PageStorageKey('trending'),
+                title: 'Trending',
+                contentList: trending,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
