eb0139f28ed5b5cbf1e7c875387c053a6d5f27d4
MuriungiMartin <MuriungiMmwiti@gmail.com>
2022-10-12 20:41:38 +0300

    UI skeleton to build on

--- server/index.js
+++ server/index.js
@@ -5,12 +5,8 @@ const e = require('express');
 
 const port = 3000;
 const app = express();
-<<<<<<< HEAD
-const DB = "mongodb+srv://Martin:Cephajosh3@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://Martin:@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
 app.use(express.json());
-=======
-const DB = "mongodb+srv://name:password@cluster0.lxlyqh4.mongodb.net/?retryWrites=true&w=majority";
->>>>>>> f7ffec078c6087b68379acc74dddf7f516488a31
 app.use(authRouter);
 mongoose.connect(DB).then(()=>{
     console.log("Connection successful");
